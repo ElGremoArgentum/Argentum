b6001a3bfee260c2f56b1d629dd371c4dc507e0e
Santiago <santiagosantuchio@gmail.com>
2025-04-23 21:02:48 -0300

    empezando

---
+++ src/main/java/argentum/Argentum.java
@@ -0,0 +1,15 @@
+package argentum;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//agregando nuevos comandos
+public class Argentum implements ModInitializer {
+	public static final String MOD_ID = "argentum";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
